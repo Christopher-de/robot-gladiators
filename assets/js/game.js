2d8c6e1da48217bc4e7b7e341114fc8d42676b60
Get Shwifty <cjpatr0n1te@icloud.com>
2021-06-22 04:09:29 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // multiple values logged at once
 console.log(playerName, playerAttack, playerHealth);
@@ -11,36 +12,62 @@ var enemyAttack = 12;
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    enemyHealth = enemyHealth - playerAttack;
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
-    );
+        // if player chose fight then fight
+        if (promptFight === "fight" || promptFight === "FIGHT") {
 
-    //   check enemy health
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died! ");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
-    }
-
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
-    );
+            //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+            enemyHealth = enemyHealth - playerAttack;
     
-    if (playerHealth <= 0) {
-        window.alert(playerName + " has died! ")
-    }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left. ");
-    }
+            console.log(
+                playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
+            );
+
+            //   check enemy health
+            if (enemyHealth <= 0) {
+                window.alert(enemyName + " has died! ");
+            }
+            else {
+                window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+            }
+    
+            // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+            playerHealth = playerHealth - enemyAttack;
+    
+            console.log(
+                enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+            );
+    
+            // check player health 
+            if (playerHealth <= 0) {
+                window.alert(playerName + " has died! ")
+            }
+            else {
+                window.alert(playerName + " still has " + playerHealth + " health left. ");
+            }
+    
+        // if player chose skip
+        } else if (promptFight === "skip" || promptFight === "SKIP") {
+            // confirm skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+
+            // if yes, leave
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // skip penalty
+                playerMoney = playerMoney - 2;
+            }
+
+            // if no, ask again by rerun fight()
+            else {
+                fight();
+            }
+            // window.alert(playerName + " has chosen to skip the fight!");
+        } else {
+            window.alert ("You need to choose a valid option. Try again!");
+        }
+        
 
 };
 
