cdf4820c96ce2a9f98314cb8c5f920d5446c7156
Get Shwifty <cjpatr0n1te@icloud.com>
2021-06-22 17:04:00 -0700

    added fight loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,22 +3,20 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// multiple values logged at once
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i)
-    console.log(enemyNames[i] + " is at " + i + " index")
-}
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat function if enemy is alive
+    while(enemyHealth > 0) {
+    // window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -75,10 +73,19 @@ var fight = function(enemyName) {
         } else {
             window.alert ("You need to choose a valid option. Try again!");
         }
-        
+    }   
 
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    // debugger;
+    var pickedEnemyName = enemyNames[i];
+
+    
+    enemyHealth = 50;
+
+    
+    fight(pickedEnemyName[i]);
+    
+    
 }
