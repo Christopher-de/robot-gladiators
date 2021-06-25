5aa78cd14ae3a064e34cdbe84456998daac89747
Get Shwifty <cjpatr0n1te@icloud.com>
2021-06-24 18:16:27 -0700

    fixed null playername issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -176,8 +176,21 @@ var shop = function() {
 };
 
 
+// set name function
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
